f27e909300ec4fc08d9f35f082b903af21af8b32
alinalinsiteweb-dev <alinalinsiteweb@gmail.com>
2026-02-14 22:37:08 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Hero from "@/components/Hero";
-import Services from "@/components/Services";
-import Portfolio from "@/components/Portfolio";
-import CTA from "@/components/CTA";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import CTA from "./components/CTA";
 
 export default function Home() {
   return (
