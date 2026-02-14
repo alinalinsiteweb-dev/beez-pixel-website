121dc49f37d2255a4253870e1e698858ee141fd1
alinalinsiteweb-dev <alinalinsiteweb@gmail.com>
2026-02-14 22:44:52 +0200

    Remove file extensions from component imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Hero from "@components/Hero.tsx";
-import Services from "@components/Services.tsx";
-import Portfolio from "@components/Portfolio.tsx";
-import CTA from "@components/CTA.tsx";
+import Hero from "@components/Hero";
+import Services from "@components/Services";
+import Portfolio from "@components/Portfolio";
+import CTA from "@components/CTA";
 
 export default function Home() {
   return (
